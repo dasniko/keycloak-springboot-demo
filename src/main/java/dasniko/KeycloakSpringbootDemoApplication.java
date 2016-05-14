f1a87515103445b61ddab24e41691e1649b10b43
Niko Köbler <niko@n-k.de>
2016-05-14 14:15:57 +0200

    initial spring boot application

---
+++ src/main/java/dasniko/KeycloakSpringbootDemoApplication.java
@@ -0,0 +1,12 @@
+package dasniko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringbootDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/dasniko/KeycloakSpringbootDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package dasniko;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KeycloakSpringbootDemoApplication.class)
+@WebAppConfiguration
+public class KeycloakSpringbootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

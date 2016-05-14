7db056260cae1316889d188688d7a9334a5aae70
Niko Köbler <niko@n-k.de>
2016-05-14 14:16:50 +0200

    add keycloak resources and injectable AccessToken beans

--- src/main/java/dasniko/KeycloakSpringbootDemoApplication.java
+++ src/main/java/dasniko/KeycloakSpringbootDemoApplication.java
@@ -1,7 +1,17 @@
 package dasniko;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.representations.AccessToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 public class KeycloakSpringbootDemoApplication {
@@ -9,4 +19,11 @@ public class KeycloakSpringbootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KeycloakSpringbootDemoApplication.class, args);
 	}
+
+	@Bean
+	@Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public AccessToken getAccessToken() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return ((KeycloakPrincipal) request.getUserPrincipal()).getKeycloakSecurityContext().getToken();
+    }
 }
